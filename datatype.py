620161716809d01977edee902b5a7a997bb2fbba
Tushar1624 <tparashar686@gmail.com>
2026-06-01 12:27:32 +0530

    Delete datatype.py

--- datatype.py
+++
@@ -1,8 +0,0 @@
-a=10
-b="Hello, World!"
-c=3.14
-d=True
-print("Value and datatype is", a, type(a))
-print("Value and datatype is", b, type(b))
-print("Value and datatype is", c, type(c))
-print("Value and datatype is", d, type(d))

0498e0c4370fca91068803da393ee211e51783dd
Tushar <tparashar686@gmail.com>
2026-06-03 12:20:18 +0530

    done

---
+++ passwordrealorfake.py
@@ -0,0 +1,6 @@
+orignal_pass="Yummy123"
+user_pass=input("Enter your password: ")
+if user_pass==orignal_pass:
+    print("Correct password Access granted")
+else:
+    print("Wrong password Access denied")

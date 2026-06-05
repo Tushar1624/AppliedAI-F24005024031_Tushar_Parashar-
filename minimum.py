fb2b0aa52c258ad5f36fffd89464d2447c7ae25f
Tushar <tparashar686@gmail.com>
2026-06-05 15:12:17 +0530

    yum

---
+++ minimum.py
@@ -0,0 +1,6 @@
+arr=[]
+num=int(input("Enter the number of values:"))
+for i in range(num):
+    n=int(input("Enter the value:"))
+    arr.append(n)
+print(min(arr))
---
+++ stringreverse.py
@@ -0,0 +1,3 @@
+n=input("Enter a word:")
+reverse=n[::-1]
+print("Orignal string=",n,"New string=",reverse)
